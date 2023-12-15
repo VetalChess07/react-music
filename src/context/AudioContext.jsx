686ace55324f24ad9f5bbd3d4219c08bd3b2e1ba
VetalChess07 <103288446+VetalChess07@users.noreply.github.com>
2023-12-15 19:14:34 +0300

    Add files via upload

--- src/context/AudioContext.jsx
+++ src/context/AudioContext.jsx
@@ -8,14 +8,72 @@ const audio = new Audio()
 
 const AudioProvider = ({children}) =>{
    // (trackLislt[0]
-   const [currentTrack, setCurrentTrack] = useState(trackList);
+   const [playerIsActive, setPlayerIsActive]= useState(false)
+   const [currentTrack, setCurrentTrack] = useState(trackList[0]);
    const [isPlaying, setIsPlaying] = useState(false);
 
+   const [countTrackIndex, setCountTrackIndex] = useState()
+
+
+  
+   const nextTrack = (track, index) =>{
+     
+     console.log(track)
+     console.log(index )
+      // if(trackList.length !== index){
+      //    handleToggleAudio(trackList[ index + 1])
+      // } else{
+      //    handleToggleAudio(trackList[0])
+      // }
+     
+      
+      
+    
+
+      
+      handleToggleAudio(trackList[index])
+        
+     
+   }
+
+   const prevTrack =( track) =>{
+      let activeTrackId = track.id - 1
+      console.log(activeTrackId)
+      if(activeTrackId !== 0){
+         handleToggleAudio(trackList[activeTrackId - 1])
+      } else{
+         handleToggleAudio(trackList[trackList.length - 1])
+      }
+   }
+
+   const randomTrack =() =>{
+      let min = 0
+      let max = trackList.length - 2
+      let rand = min + Math.random() * (max + 1 - min);
+      
+     alert( Math.floor(rand))
+    return  handleToggleAudio(trackList[Math.floor(rand)])
+   }
+
+   audio.onended = function(){
+     if(currentTrack.id  === trackList.length) {
+    
+     audio.pause()
+     alert(`Вы прослушали все ${trackList.length} композиций `)
+       // тут ещё код вывода названия песни, оно находится в tracklist[index].name
+    } else{
+      handleToggleAudio(trackList[currentTrack.id]);
+    } 
+   }
+   audio.volume =0.1
    const handleToggleAudio =(track)=>{
-
-      console.log(track)
-
-      if(currentTrack.id !== track.id){
+      // включение плеера
+     
+      
+      setPlayerIsActive(true)
+     
+     
+         if(currentTrack.id !== track.id){
          setCurrentTrack(track)
          setIsPlaying(true)
 
@@ -33,9 +91,14 @@ const AudioProvider = ({children}) =>{
          audio.play()
          setIsPlaying(true)
       }
+      
+       
+      
+      
+
    };
 
-   const value ={audio ,currentTrack, isPlaying, handleToggleAudio}
+   const value ={audio ,currentTrack, isPlaying, handleToggleAudio, nextTrack, prevTrack, playerIsActive, randomTrack}
 
    return <AudioContext.Provider value={value}>{children}</AudioContext.Provider>
 
